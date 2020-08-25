d1a06f0945f19bcc45ce424e23f40f968874290d
Виктор <nerevaren@mail.ru>
2020-08-25 20:50:12 +0300

    Add files via upload

---
+++ _bot_.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import requests
+
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ssl2/cert.key -out ssl2/cert.crt
+acc_key = "1324149285:AAFEk04wDnLHV8279klY1H7zyUx46_X4dkM" 
+files = {'certificate': open('ssl/cert.crt', 'rb')}
+
+#------------------------->
+my_l = "https://api.telegram.org/bot"+acc_key+"/setWebhook?url=https://178.158.131.41:8443"
+r = requests.post(my_l, files=files)
+print (r.text)
+#---------------->
+my_l = "https://api.telegram.org/bot"+acc_key+"/getWebhookInfo"
+r = requests.get(my_l)
+print (r.text)
+#---------------->
+#DELETE
+#my_l = "https://api.telegram.org/bot"+acc_key+"/deleteWebhook"
+#r = requests.get(my_l)
+#print (r.text)
+  
+
+
+
